fd4401f8c281a55e83da959bf8e24541db27c8f5
Igor Osipov <ighor.osipov.95@mail.ru>
2022-03-22 20:04:40 +0300

    Add LoginFragment and binding with mobile_navigation.xml

--- app/src/main/java/com/example/webfactory/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/webfactory/ui/login/LoginFragment.java
@@ -1,32 +1,52 @@
 package com.example.webfactory.ui.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.webfactory.MainActivity;
+import com.example.webfactory.R;
 import com.example.webfactory.databinding.FragmentLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginFragment extends AppCompatActivity {
+import java.util.Objects;
+
+public class LoginFragment extends Fragment {
     private FragmentLoginBinding binding;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private NavController navController;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = FragmentLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentLoginBinding.bind(view);
+        navController = Navigation.findNavController(view);
         mAuth = FirebaseAuth.getInstance();
 
         binding.login.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +65,9 @@ public class LoginFragment extends AppCompatActivity {
 
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null) {
-            Intent intent = new Intent(LoginFragment.this, MainActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(getContext(), MainActivity.class);
+//            startActivity(intent);
+            //navController.navigate(R.id.action_loginFragment_to_nav_home);
         }
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -54,8 +75,7 @@ public class LoginFragment extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(LoginFragment.this, MainActivity.class);
-                    startActivity(intent);
+                    //navController.navigate(R.id.action_loginFragment_to_nav_home);
                 } else {
 
                 }
@@ -64,28 +84,38 @@ public class LoginFragment extends AppCompatActivity {
     }
 
     private void login(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(LoginFragment.this, "Авторизация прошла успешно!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "Авторизация прошла успешно!", Toast.LENGTH_SHORT).show();
+                    navController.navigate(R.id.action_loginFragment_to_nav_home);
                 } else {
-                    Toast.makeText(LoginFragment.this, "Авторизация провалена!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "Авторизация провалена!", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
     }
 
         private void register(String email, String password){
-            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        Toast.makeText(LoginFragment.this, "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show();
                     }else{
-                        Toast.makeText(LoginFragment.this, "Регистрация провалена!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Регистрация провалена!", Toast.LENGTH_SHORT).show();
+
                     }
                 }
             });
         }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
     }
