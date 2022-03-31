d02efa6c20bf4287786317fa057903742cd79820
Igor Osipov <ighor.osipov.95@mail.ru>
2022-03-31 17:18:20 +0300

    Add delete calendar user in CalendarFragment

--- app/src/main/java/com/example/webfactory/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/webfactory/ui/calendar/CalendarFragment.java
@@ -25,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.ParseException;
@@ -34,12 +35,14 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 
 public class CalendarFragment extends Fragment {
 
     private FragmentCalendarBinding binding;
     private List<EventDay> mEventDays;
+    private List<String> calendarUserKeys;
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
     private FirebaseUser user;
@@ -60,24 +63,24 @@ public class CalendarFragment extends Fragment {
 
         mEventDays = new ArrayList<>();
 
-
         myRef.child(user.getUid()).child("calendar").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 List<CalendarUser> calendarUserList = new ArrayList<>();
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                calendarUserKeys = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     CalendarUser calendarUser = dataSnapshot.getValue(CalendarUser.class);
                     calendarUserList.add(calendarUser);
+                    calendarUserKeys.add(dataSnapshot.getKey());
                 }
                 calendarUserList.forEach((CalendarUser name) -> {
                     SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZ yyyy", Locale.ENGLISH);
                     try {
                         Date date = format.parse(name.getDate());
-                        Toast.makeText(getContext(), date.toString(), Toast.LENGTH_SHORT).show();
                         Calendar calendar = Calendar.getInstance();
                         calendar.setTime(date);
                         mEventDays.add(new EventDay(calendar, R.drawable.ic_menu_camera));
-                       binding.calendarView.setEvents(mEventDays);
+                        binding.calendarView.setEvents(mEventDays);
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
@@ -91,8 +94,6 @@ public class CalendarFragment extends Fragment {
         });
 
 
-
-
         binding.calendarView.setOnDayClickListener(new OnDayClickListener() {
             @Override
             public void onDayClick(EventDay eventDay) {
@@ -103,6 +104,7 @@ public class CalendarFragment extends Fragment {
         return root;
     }
 
+
     private void addWorkerOnDate(EventDay eventDay) {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(requireContext());
         alertDialog.setPositiveButton("Добавить сотрудников", new DialogInterface.OnClickListener() {
@@ -112,8 +114,6 @@ public class CalendarFragment extends Fragment {
                 CalendarUser calendarUser = new CalendarUser();
                 calendarUser.setDate(clickedDayCalendar.getTime().toString());
                 myRef.child(user.getUid()).child("calendar").push().setValue(calendarUser);
-                mEventDays.add(new EventDay(clickedDayCalendar, R.drawable.ic_menu_camera));
-                binding.calendarView.setEvents(mEventDays);
                 Toast.makeText(getContext(), "Сотрудники добавлены", Toast.LENGTH_SHORT).show();
             }
         });
@@ -122,9 +122,9 @@ public class CalendarFragment extends Fragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Calendar clickedDayCalendar = eventDay.getCalendar();
-                mEventDays.remove(eventDay);
-                binding.calendarView.setEvents(mEventDays);
-                Toast.makeText(getContext(), "Сотрудники удалены", Toast.LENGTH_SHORT).show();
+                CalendarUser calendarUser = new CalendarUser();
+                calendarUser.setDate(clickedDayCalendar.getTime().toString());
+                delete(calendarUser, clickedDayCalendar);
             }
         });
 
@@ -139,12 +139,36 @@ public class CalendarFragment extends Fragment {
 
     }
 
+    private void delete(CalendarUser calendarUser, Calendar clickedDayCalendar) {
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference().child(user.getUid()).child("calendar");
+        for (int i = 0; i < calendarUserKeys.size(); i++) {
+            Query query = dbref.child(calendarUserKeys.get(i));
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.child("date").getValue(String.class).equals(calendarUser.getDate())) {
+                        dataSnapshot.getRef().removeValue();
+                        mEventDays = new ArrayList<>();
+                        binding.calendarView.setEvents(mEventDays);
+                        Toast.makeText(getContext(), "Сотрудники удалены", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
 
 
 }
