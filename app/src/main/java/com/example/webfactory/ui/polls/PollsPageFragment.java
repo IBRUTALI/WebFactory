e4d9b1b7e9f5dbcebcef9d90cef0a8efc14f8732
Igor Osipov <ighor.osipov.95@mail.ru>
2022-04-16 17:22:57 +0300

    List View for polls page

--- app/src/main/java/com/example/webfactory/ui/polls/PollsPageFragment.java
+++ app/src/main/java/com/example/webfactory/ui/polls/PollsPageFragment.java
@@ -4,20 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.webfactory.R;
 import com.example.webfactory.databinding.FragmentPollsPageBinding;
 
+import java.util.ArrayList;
+
 public class PollsPageFragment extends Fragment {
     private FragmentPollsPageBinding binding;
+    private ArrayList<String> varList;
     private String id, title, var1, var2, var3;
+    private NavController navController;
 
 
     public PollsPageFragment() {
         super(R.layout.fragment_polls_page);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,6 +40,13 @@ public class PollsPageFragment extends Fragment {
         binding = FragmentPollsPageBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         getAndSetIntentData();
+
+        binding.sendPolls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPolls();
+            }
+        });
         return root;
     }
 
@@ -39,20 +61,21 @@ public class PollsPageFragment extends Fragment {
             var3 = getArguments().getString("pollsVar3");
 
             binding.pollsPageTitle.setText(title);
-            binding.pollsPageVar1.setText(var1);
-            binding.pollsPageVar2.setText(var2);
-            binding.pollsPageVar3.setText(var3);
+            varList = new ArrayList<>();
+            varList.add(var1);
+            varList.add(var2);
+            varList.add(var3);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.polls_list_item, R.id.pollsPageTVItem, varList);
+            binding.pollsPageLV.setAdapter(arrayAdapter);
         } else {
             Toast.makeText(getContext(), "Нет данных", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void sendPolls() {
-        // TODO: 01.03.2022 Возможность отправки результатов анкетирования с сохранением в БД 
-    }
-
-    private void deletePolls() {
-        // TODO: 01.03.2022 Возможность удаление анкеты 
+        // TODO: 01.03.2022 Возможность отправки результатов анкетирования с сохранением в БД
+        Toast.makeText(getContext(), "Анкета отправлена!", Toast.LENGTH_SHORT).show();
+        navController.popBackStack();
     }
 
     @Override
